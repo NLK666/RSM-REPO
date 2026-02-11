382f40b0d5b9f0af5f4cbf7673b78e2c4beebc18
NLK666 <110420544+NLK666@users.noreply.github.com>
2026-02-11 14:18:20 +0100

    Mis a jours

---
+++ JoffreyV1.py
@@ -0,0 +1,39 @@
+from cryptography.fernet import Fernet
+import glob
+
+Choix = input(" Choisisez votre mode 1 (Crypter) ou 2 pour (Déscrypter) : ")
+if Choix == "1" :
+
+    ## Generation de la clef ##
+    user_key = input("Générer une nouvelle clé (o/n) ")
+
+    if user_key.lower() == 'o':
+        user_key = Fernet.generate_key()
+        with open('key.key', 'wb') as f:
+            f.write(user_key)
+    else:
+        with open('key.key', 'rb') as f:
+            user_key = f.read()
+        
+    ## Encryptage ##
+    files = glob.glob("*.txt")
+    for file in files:
+        with open(file, 'rb') as f:
+            data = f.read()
+        fernet = Fernet(user_key)
+        encrypted = fernet.encrypt(data)
+        with open(file, 'wb') as f:
+            f.write(encrypted)
+
+## Décryptage ##
+if Choix == "2" :
+    decriptefil = input("Entrez la clef de décryptage (entrez pour ignorer) : ")
+
+    file = glob.glob('*.txt')
+    for files in file:
+        with open(files, 'rb') as f:
+            data = f.read()
+        fernet = Fernet(decriptefil)
+        decrypted = fernet.decrypt(data)
+        with open(files, 'wb') as f:
+            f.write(decrypted)
